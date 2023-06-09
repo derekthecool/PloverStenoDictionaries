ba69d587ebd3ca636d0bb928b2ef3909e36454f8
Derek Lomax <derekthecool@gmail.com>
2023-06-09 16:26:34 -0600

    Add faster and more reliable version of get_offline_snippet.py

--- plover/get_offline_snippet.py
+++ plover/get_offline_snippet.py
@@ -6,19 +6,28 @@ Purpose: Pull file from local file system
 import os.path
 import pathlib
 import sys
+import subprocess
 
 # Length of the longest supported key (number of strokes).
 LONGEST_KEY = 2
 
 platform = sys.platform
 if platform == "win32":
-    BASE_PATH = os.path.join(os.getenv("LOCALAPPDATA"), "plover", "plover")
+    plover_base_path = os.path.join(os.getenv("LOCALAPPDATA"), "plover", "plover")
+    clipboard_command = 'clip'
 elif platform == "linux":
-    BASE_PATH = os.path.join(os.getenv("HOME"), ".config", "plover", "plover")
+    plover_base_path = os.path.join(os.getenv("HOME"), ".config", "plover", "plover")
+    clipboard_command = 'pbcopy'
+elif platform == "darwin":
+    # Never tested
+    plover_base_path = os.path.join(os.getenv("HOME"), "Library", "Application Support", "plover")
+    clipboard_command = 'pbcopy'
+
 
 online_snippet_dictionary = {
     # lua
     "HRAOU": "clash-of-code.lua",
+    "TPUPB": "lua-fun-minified.lua",
 
     # C#
     "SHA*RP": "clash-of-code.cs",
@@ -42,13 +51,18 @@ def lookup(key):
 
     if key[0] == "TP*ET" and len(key) > 1:
         filename = os.path.join(
-            BASE_PATH, "local_snippets", online_snippet_dictionary.get(key[1])
+            plover_base_path, "local_snippets", online_snippet_dictionary.get(key[1])
         )
         exists = os.path.isfile(filename)
         # return str(exists)
         if exists:
             f = open(filename, "r")
-            return f.read()
+
+            # Setting the clipboard is faster than Plover "typing" out the text
+            # Longer texts get mangled when printed by Plover
+            # Make sure to set shell=True or it'll open a terminal window briefly
+            subprocess.run(clipboard_command, text=True, input=f.read(), shell=True)
+            return '{#Control_L(v)}'
         else:
             raise KeyError
     else:
