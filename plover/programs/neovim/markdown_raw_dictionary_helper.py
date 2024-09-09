256307c975a3d454a83756e2f3ca7f489b5e897a
Derek Lomax <derekthecool@gmail.com>
2024-09-09 11:56:55 -0600

    refactor(neovim): move more neovim related items into neovim directory

--- plover/programs/neovim/markdown_raw_dictionary_helper.py
+++ plover/programs/neovim/markdown_raw_dictionary_helper.py
@@ -5,10 +5,10 @@ Requirements:
     - python dictionary plugin (https://pypi.org/project/plover-python-dictionary/)
 """
 
-LONGEST_KEY = 7
+LONGEST_KEY = 10
 START_SEQUENCE = "RAUS"
-STOP_SEQUENCE = "STOP"
-started = False
+# My Emilys symbols mapping for a colon with spaces on both sides ' : '
+STOP_SEQUENCE = "SKWHAOLG"
 
 
 def lookup(key):
@@ -16,18 +16,13 @@ def lookup(key):
 
     if key[0] != START_SEQUENCE:
         raise KeyError
-    elif key[0] == START_SEQUENCE and len(key) == 1:
+
+    # Print a help prompt when entering this mode
+    # After sending any other keys the prompt deletes
+    if key[0] == START_SEQUENCE and len(key) == 1:
         return "Enter raw steno string and have it joined with '/' STOP=stop"
-    else:
-        started = True
 
     if STOP_SEQUENCE in str(key[1:]):
-        started = False
-        # raise KeyError
-        return "/".join(key[1:-1])
-        # return ""
-
-    if started:
-        return "/".join(key[1:])
-    else:
         raise KeyError
+
+    return "/".join(key[1:])
